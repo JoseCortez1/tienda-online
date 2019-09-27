18c992ba10109d6d639537c50fc4bee05dd585ad
JoseCortez1 <jose.cucei96@gmail.com>
2019-09-27 10:56:18 -0500

    Se cambia a un funcion a parte la validacion del formato del archivo

--- js/script.js
+++ js/script.js
@@ -9,21 +9,31 @@ let stateCheck = setInterval(() => {
         form.addEventListener("blur", function( event ) {  //listener blur para evaluar el campo una vez fuera de el 
           
           let dato = event.target.value;
-          if(dato.trim() != ''){//quitando los campos vacioss 
-            if(event.target.getAttribute('id') === 'imagen_archivo'){  //Validando que sea el input de imagen
-              console.log("this is the document");
-              let extension = dato.split('.').pop();  //Separando la extencion para validar
-              if(extension == 'jpg'){   //Validando la extension correcta
-                console.log("it is a valid format");
-              }else{
-                console.log("it isn't valid format");
-              }
-            }
+          if(dato.trim() != ''){
+            //the target has data
+            correctFormat(event.target);
+
+ 
           }else{
-            console.log("sin datos");
+            //the target doesn't have data
           }
           
         }, true);
+
+
+        function correctFormat(element){
+          console.log(element);
+          let dato = element.value;
+          if(element.getAttribute('id') === 'imagen_archivo'){  //Validando que sea el input de imagen
+            console.log("this is the document");
+            let extension = dato.split('.').pop();  //Separando la extencion para validar
+            if(extension == 'jpg'){   //Validando la extension correcta
+              console.log("it is a valid format");
+            }else{
+              console.log("it isn't valid format");
+            }
+          }
+        }
 
     }
   }, 100);
