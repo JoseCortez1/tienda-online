f866a894891981fcecf43c95f1daeaa3d2778bdf
JoseCortez1 <jose.cucei96@gmail.com>
2019-09-27 10:21:28 -0500

    Lee los datos una vez dejada la casilla

--- js/script.js
+++ js/script.js
@@ -2,21 +2,29 @@ console.log("Antes");
 let stateCheck = setInterval(() => {
     if (document.readyState === 'complete') {
       clearInterval(stateCheck);
-      // document ready
-      console.log("Cargado 1");
+        // document ready
+        console.log("Cargado 1");
 
-    let nombre = document.querySelector('#nombre');
-    let apellido = document.querySelector('#apellido');
-    let correo = document.querySelector('#correo');
-    let password = document.querySelector('#password');
+        let nombre = document.querySelector('#nombre');
+        let apellido = document.querySelector('#apellido');
+        let correo = document.querySelector('#correo');
+        let password = document.querySelector('#password');
 
-    let rol = document.querySelector('#rol');
-    let imagen_archivo = document.querySelector('#imagen_archivo');
+        let rol = document.querySelector('#rol');
+        let imagen_archivo = document.querySelector('#imagen_archivo');
 
-    var form = document.querySelector("form");
-    form.addEventListener("focus", function( event ) {
-    event.target.style.background = "pink";    
-    }, true);
+        var form = document.querySelector("form");
+        form.addEventListener("blur", function( event ) {
+          
+          console.log(event.target.value);
+          let dato = event.target.value;
+          if(dato.trim() != ''){
+            console.log("con datos");
+          }else{
+            console.log("sin datos");
+          }
+          
+        }, true);
 
     }
   }, 100);
