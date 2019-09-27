5fb74269cc804bbf7b7090c21560c55ae1ebcdc5
JoseCortez1 <jose.cucei96@gmail.com>
2019-09-27 11:12:05 -0500

    se añaden la funciones para enfocar los campos con error(datos no llenados o error)

--- js/script.js
+++ js/script.js
@@ -11,28 +11,38 @@ let stateCheck = setInterval(() => {
           let dato = event.target.value;
           if(dato.trim() != ''){
             //the target has data
+            notEmptyData(event.target);
             correctFormat(event.target);
-
  
           }else{
             //the target doesn't have data
+            emptyData(event.target);
           }
           
         }, true);
 
-
+        //funciones
         function correctFormat(element){
-          console.log(element);
           let dato = element.value;
           if(element.getAttribute('id') === 'imagen_archivo'){  //Validando que sea el input de imagen
+            
             console.log("this is the document");
             let extension = dato.split('.').pop();  //Separando la extencion para validar
             if(extension == 'jpg'){   //Validando la extension correcta
               console.log("it is a valid format");
+              notEmptyData(element);
             }else{
               console.log("it isn't valid format");
+              emptyData(element);
             }
           }
+        }
+
+        function emptyData(element){
+          element.classList.add('error');
+        }
+        function notEmptyData(element){
+          element.classList.remove('error');
         }
 
     }
